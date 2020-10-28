888f1559587d76bcd23ce849fbc66cb4d24627cf
jerry <mys16653122004@gmail.com>
2020-10-28 16:12:34 +0800

    增加创建configmap 并且挂载到/mnt目录下

--- controllers/controller_tools.go
+++ controllers/controller_tools.go
@@ -2,85 +2,208 @@ package controllers
 
 import (
 	"context"
-	"github.com/go-logr/logr"
 	"github.com/presslabs/controller-util/syncer"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/tools/record"
 	nginxv1 "nginx-operator/api/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type deploySyncer struct {
-	nginx 		*nginxv1.Nginx
+	nginx *nginxv1.Nginx
 }
 
-func (s *deploySyncer) SyncFn(in runtime.Object,log logr.Logger) error {
+func (r *NginxReconciler) ConfigMapSyncer(nginx *nginxv1.Nginx) syncer.Interface {
+	cm := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      nginx.Name,
+			Namespace: nginx.Namespace,
+			Labels: map[string]string{
+				"app":  "configmap",
+				"name": nginx.Name,
+			},
+		},
+		Data: NginxDefaultSettings(),
+	}
+
+	return syncer.NewObjectSyncer("ConfigMap", nginx, cm, r.Client, r.Scheme, func() error {
+		return nil
+	})
+}
+
+func (s *deploySyncer) ensureVolumes() []corev1.Volume {
+	fileMode := int32(0644)
+	volumes := []corev1.Volume{
+		{
+			Name: s.nginx.Name,
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: s.nginx.Name,
+					},
+					DefaultMode: &fileMode,
+				},
+			},
+		},
+	}
+	return volumes
+}
+
+func (s *deploySyncer) ensureContainers() []corev1.Container {
+	containers := []corev1.Container{
+		{
+			Name:  s.nginx.Name,
+			Image: s.nginx.Spec.Image,
+			VolumeMounts: []corev1.VolumeMount{
+				{Name: s.nginx.Name, MountPath: "/mnt/"},
+			},
+		},
+	}
+	return containers
+}
+
+func (s *deploySyncer) ensurePodSpec() corev1.PodSpec {
+	podSpec := corev1.PodSpec{
+		Volumes:    s.ensureVolumes(),
+		Containers: s.ensureContainers(),
+	}
+	return podSpec
+}
+
+func (s *deploySyncer) DeploymentSyncFn(in runtime.Object) error {
 	out := in.(*appsv1.Deployment)
 	out.Spec.Replicas = &s.nginx.Spec.Size
 	out.Spec.Selector = metav1.SetAsLabelSelector(labels.Set{
-		"app": "nginx",
+		"app":  "nginx",
 		"name": s.nginx.Name,
 	})
 	out.Spec.Template.ObjectMeta.Labels = map[string]string{
-		"app": "nginx",
+		"app":  "nginx",
 		"name": s.nginx.Name,
 	}
+	out.Spec.Template.Spec = s.ensurePodSpec()
 
 	s.nginx.Status.ReadyNodes = out.Status.ReadyReplicas
-	log.Info("** Syncer ReadyNodes **", "Status", out.ObjectMeta.Generation)
-
 	return nil
 }
 
-func NewDeploymentSyncer(c client.Client, scheme *runtime.Scheme, nginx *nginxv1.Nginx, log logr.Logger) syncer.Interface {
-	obj  := &appsv1.Deployment{
+func (r *NginxReconciler) NewDeploymentSyncer(nginx *nginxv1.Nginx) syncer.Interface {
+	obj := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:                       nginx.Name,
-			Namespace:                  nginx.Namespace,
+			Name:      nginx.Name,
+			Namespace: nginx.Namespace,
 		},
-		Spec:	appsv1.DeploymentSpec{
+		Spec: appsv1.DeploymentSpec{
 			Replicas: &nginx.Spec.Size,
 			Selector: &metav1.LabelSelector{
-				MatchLabels:      map[string]string{
-					"app": "nginx",
+				MatchLabels: map[string]string{
+					"app":  "nginx",
 					"name": nginx.Name,
 				},
 				MatchExpressions: nil,
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:         nginx.Name,
-					Namespace:    nginx.Namespace,
+					Name:      nginx.Name,
+					Namespace: nginx.Namespace,
 				},
-				Spec:       corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  nginx.Name,
-							Image: nginx.Spec.Image,
-						},
-					},
-				},
+				Spec: corev1.PodSpec{},
 			},
 		},
 	}
-	sync := &deploySyncer {
+	sync := &deploySyncer{
 		nginx: nginx,
 	}
-	return syncer.NewObjectSyncer("Deployment", nginx, obj, c, scheme, func() error {
-		return sync.SyncFn(obj, log)
+	return syncer.NewObjectSyncer("Deployment", nginx, obj, r.Client, r.Scheme, func() error {
+		return sync.DeploymentSyncFn(obj)
 	})
 }
 
-func (r *NginxReconciler) Sync (s syncer.Interface,log logr.Logger, recorder record.EventRecorder) (syncer.SyncResult, error){
+func (r *NginxReconciler) Sync(s syncer.Interface) (syncer.SyncResult, error) {
 	// 进行更新
 	result, err := s.Sync(context.Background())
 	owner := s.GetOwner()
-	if recorder != nil && owner != nil && result.EventType != "" && result.EventReason != "" && result.EventMessage != "" {
-		recorder.Eventf(owner, result.EventType, result.EventReason, result.EventMessage)
+	if r.Recorder != nil && owner != nil && result.EventType != "" && result.EventReason != "" && result.EventMessage != "" {
+		r.Recorder.Eventf(owner, result.EventType, result.EventReason, result.EventMessage)
 	}
 	return result, err
+}
+
+func (r *NginxReconciler) GetPodList(nginxCR *nginxv1.Nginx) []string {
+	listOpts := []client.ListOption{
+		client.InNamespace(nginxCR.Namespace),
+		client.MatchingLabels(labelsForNginxd(nginxCR.Name)),
+	}
+	podList := &corev1.PodList{}
+	if err := r.List(context.TODO(), podList, listOpts...); err != nil {
+		r.Log.Error(err, "Failed to get Pod List")
+	}
+	podNames := []string{}
+	for _, pod := range podList.Items {
+		r.Log.Info("Pod", "podItem", pod.Name)
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
+}
+
+func labelsForNginxd(name string) map[string]string {
+	return map[string]string{"app": "nginx", "name": name}
+}
+
+func NginxDefaultSettings() map[string]string {
+	return map[string]string{
+		"default": `server {
+    listen       80;
+    listen  [::]:80;
+    server_name  localhost;
+
+    #charset koi8-r;
+    #access_log  /var/log/nginx/host.access.log  main;
+	#abcdefaffjawefakgdsfa
+
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+    }
+
+    #error_page  404              /404.html;
+
+    # redirect server error pages to the static page /50x.html
+    #
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+
+    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+    #
+    #location ~ \.php$ {
+    #    proxy_pass   http://127.0.0.1;
+    #}
+
+    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+    #
+    #location ~ \.php$ {
+    #    root           html;
+    #    fastcgi_pass   127.0.0.1:9000;
+    #    fastcgi_index  index.php;
+    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+    #    include        fastcgi_params;
+    #}
+
+    # deny access to .htaccess files, if Apache's document root
+    # concurs with nginx's one
+    #
+    #location ~ /\.ht {
+    #    deny  all;
+    #}
+}`,
+	}
 }
--- controllers/nginx_controller.go
+++ controllers/nginx_controller.go
@@ -19,27 +19,25 @@ package controllers
 import (
 	"context"
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	nginxv1 "nginx-operator/api/v1"
 	"time"
+
+	//"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	nginxv1 "nginx-operator/api/v1"
 )
 
 // NginxReconciler reconciles a Nginx object
 type NginxReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log      logr.Logger
+	Scheme   *runtime.Scheme
 	Recorder record.EventRecorder
 }
-
-
-
-
 
 // +kubebuilder:rbac:groups=nginx.example.com,resources=nginxes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=nginx.example.com,resources=nginxes/status,verbs=get;update;patch
@@ -60,28 +58,50 @@ func (r *NginxReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, err
 	}
 
-	deploymentSync := NewDeploymentSyncer(r.Client, r.Scheme, nginxCR, log)
-	result, err := r.Sync(deploymentSync, log, r.Recorder)
-	//if err = syncer.Sync(ctx, deploymentSync, r.recorder); err != nil {
-	//	return ctrl.Result{}, err
-	//}
+	cm := r.ConfigMapSyncer(nginxCR)
+	_, _ = r.Sync(cm)
 
-	log.Info("** Result Type **", "TYPE", result.Operation)
-	if result.Operation == controllerutil.OperationResultNone{
-		log.Info("** Update Status **")
-		err := r.Status().Update(context.TODO(), nginxCR)
+	// 1. 使用自己写的Sync方法，而不用模块中的Sync方法，是因为模块中的Sync方法只会返回一个
+	// error， 所以我们拿不到Sync的中间结果， 所以我们重写Sync方法，将其结果也返回出来
+	// 2. 为什么要重写这个方法？
+	// 在更新yaml文件之后， 需要修改 Status 状态，但是有时候，缓存同步的慢，例如我们通过获取
+	// deployment的ready replicas是无法获取当前正确的副本数的，我们获取到的是上一次的副本数
+	// 所以这里我们通过判断对deployment的操作（update，create），如果是这两个操作，进行
+	// 一次requeue
+	// if err = syncer.Sync(ctx, deploymentSync, r.recorder); err != nil {
+	//	return ctrl.Result{}, err
+	// }
+	deploymentSync := r.NewDeploymentSyncer(nginxCR)
+	_, _ = r.Sync(deploymentSync)
+
+	podList := r.GetPodList(nginxCR)
+	nginxCR.Status.Nodes = podList
+	if len(podList) > int(nginxCR.Status.ReadyNodes) {
+		return ctrl.Result{RequeueAfter: 4 * time.Second}, nil
+	} else {
+		log.Info("Update Status", "Nodes", nginxCR.Status.Nodes)
+		err = r.Status().Update(context.TODO(), nginxCR)
 		if err != nil {
 			log.Error(err, "Failed to update cluster status")
 		}
-	} else {
-		return ctrl.Result{RequeueAfter: 4*time.Second}, nil
 	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *NginxReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&nginxv1.Nginx{}).
-		Complete(r)
+	// ctrl.NewControllerManagerdBy 会返回一个Builder的结构体
+	// For方法将 &nginxv1.Nginx{} 赋值给 Builer.forInput
+	blder := ctrl.NewControllerManagedBy(mgr).For(&nginxv1.Nginx{})
+
+	// Owner 将 &appsv1.Deployment 传给 Builer.OwnsInput 后面wath的时候会用
+	// 为什么要 watch Deployment ?
+	// 如果不 wathch Deployment 的话， 当手动修改 Deployment 信息，例如副本数的时候，
+	// 他是不会发觉的，如果你做了 watch 操作， 这个deployment的副本数会始终和你的CR的
+	// 副本数是一致的
+	blder.Owns(&appsv1.Deployment{})
+
+	// 创建一个 Controller，然后 watch nginxv1.Nginx 和 上面的 appsv1.Deployment
+	_, err := blder.Build(r)
+	return err
 }
